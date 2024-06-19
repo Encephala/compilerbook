e441eb5dc082aa7be374b8cfa94584c8e261cd84
Encephala <jonathan_rietveld@hotmail.com>
2024-06-19 17:48:34 +0200

    Add bytecode disassembler

--- src/monkey/opcode/opcode.go
+++ src/monkey/opcode/opcode.go
@@ -1,12 +1,46 @@
 package opcode
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
 type Instruction []byte
 type Instructions []byte
+
+func (instructions Instructions) String() string {
+	var out bytes.Buffer
+
+	offset := 0
+	for offset < len(instructions) {
+		definition, err := Lookup(OpCode(instructions[offset]))
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(definition, instructions[offset+1:])
+
+		fmt.Fprintf(
+			&out, "%04d %s\n",
+			offset, fmtInstruction(definition, operands),
+		)
+
+		offset += 1 + int(read)
+	}
+
+	return out.String()
+}
+
+func fmtInstruction(definition *OpDefinition, operands []uint) string {
+	switch len(operands) {
+	case 1:
+		return fmt.Sprintf("%s %d", definition.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", definition.Name)
+}
 
 type OpCode byte
 
@@ -44,7 +78,7 @@ func Make(code OpCode, operands ...uint) Instruction {
 		// To be fair (as book points out)
 		// Only matters for testing/debugging, as we're the only ones actually calling this function
 		// Tests will catch this faulty behaviour
-		return []byte{}
+		return Instruction{}
 	}
 
 	var instructionsLength uint = 1
@@ -70,4 +104,22 @@ func Make(code OpCode, operands ...uint) Instruction {
 	}
 
 	return result
+}
+
+func ReadOperands(definition *OpDefinition, raw_operands []byte) ([]uint, uint) {
+	operands := make([]uint, len(definition.OperandWidths))
+
+	var offset uint = 0
+	for i, width := range definition.OperandWidths {
+		switch width {
+		case 1:
+			operands[i] = uint(raw_operands[offset])
+		case 2:
+			operands[i] = uint(binary.BigEndian.Uint16(raw_operands[offset:]))
+		}
+
+		offset += width
+	}
+
+	return operands, offset
 }
--- src/monkey/opcode/opcode_test.go
+++ src/monkey/opcode/opcode_test.go
@@ -31,3 +31,64 @@ func TestMake(t *testing.T) {
 		}
 	}
 }
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []Instruction{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatenated := Instructions{}
+	for _, instruction := range instructions {
+		concatenated = append(concatenated, instruction...)
+	}
+
+	if concatenated.String() != expected {
+		t.Fatalf(
+			"Wrong string %q, expected %q\n",
+			concatenated.String(), expected,
+		)
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		code      OpCode
+		operands  []uint
+		bytesRead uint
+	}{
+		{OpConstant, []uint{65535}, 2},
+	}
+
+	for _, test := range tests {
+		instruction := Make(test.code, test.operands...)
+
+		definition, err := Lookup(test.code)
+		if err != nil {
+			t.Fatalf("definition not found: %q\n", err)
+		}
+
+		operandsRead, bytesRead := ReadOperands(definition, instruction[1:])
+		if bytesRead != test.bytesRead {
+			t.Fatalf(
+				"Number of bytes %d wrong, expected %d",
+				bytesRead, test.bytesRead,
+			)
+		}
+
+		for i, expected := range operandsRead {
+			if operandsRead[i] != expected {
+				t.Errorf(
+					"wrong operand %d, expected %d",
+					operandsRead[i], expected,
+				)
+			}
+		}
+	}
+}

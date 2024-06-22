54317ad32009fb53fea2185b17a8bae87e40391e
Encephala <jonathan_rietveld@hotmail.com>
2024-06-22 20:41:38 +0200

    Compile integer constants

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -24,7 +24,48 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, statement := range node.Statements {
+			err := c.Compile(statement)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+
+		c.addConstant(integer)
+	}
+
 	return nil
+}
+
+func (c *Compiler) addConstant(constant object.Object) {
+	index := len(c.constants)
+
+	bytecode := opcode.Make(opcode.OpConstant, uint(index))
+
+	c.constants = append(c.constants, constant)
+
+	for _, b := range bytecode {
+		c.instructions = append(c.instructions, b)
+	}
 }
 
 func (c *Compiler) Bytecode() *Bytecode {
